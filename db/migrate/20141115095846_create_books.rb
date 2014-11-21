6354507268a3e41c5316c2a596eddfe2e6f4fd38
Bhushan Lodha <bhushanlodha@gmail.com>
2014-11-21 15:05:45 +0530

    Added rest of the migrations

---
+++ db/migrate/20141115095846_create_books.rb
@@ -0,0 +1,19 @@
+class CreateBooks < ActiveRecord::Migration
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :authors
+      t.string :mrp
+
+      t.references :university
+      t.references :college
+      t.references :department
+      t.references :course
+      t.references :semester
+      t.references :subject
+      t.references :publication
+
+      t.timestamps
+    end
+  end
+end
