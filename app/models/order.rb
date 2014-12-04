81e35550e9520590394070b191bc89ce57a40589
Bhushan Lodha <bhushanlodha@gmail.com>
2014-12-04 16:58:58 +0530

    Order synced

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,14 +6,14 @@ class Order < ActiveRecord::Base
   belongs_to :handler, class_name: 'AdminUser'
 
   enum status: {
-                "order placed" =>     0,
+                "order placed"     => 0,
                 "handler assigned" => 1,
                 "seller confirmed" => 2,
-                "buyer confirmed" =>  3,
-                "item picked" =>      4,
-                "item delivered" =>   5,
-                "payment pending" =>  6,
-                "order cancelled" =>  7
+                "buyer confirmed"  => 3,
+                "item picked"      => 4,
+                "item delivered"   => 5,
+                "payment pending"  => 6,
+                "cancelled"        => 7
                }
 
   def order_placed?
@@ -22,6 +22,24 @@ class Order < ActiveRecord::Base
 
   def handler_assigned?
     status == "handler assigned"
+  end
+
+  def serialized_hash(options = {})
+    data = {}
+
+    data[:id]                  = id
+    data[:handler_assigned_at] = handler_assigned_at.try(:to_s)
+    data[:seller_confirmed_at] = seller_confirmed_at.try(:to_s)
+    data[:item_picked_at]      = item_picked_at.try(:to_s)
+    data[:status]              = status
+    data[:created_at]          = created_at.to_s
+    data[:updated_at]          = updated_at.to_s
+
+    data[:listing]         = {}
+    data[:listing][:id]    = listing.id
+    data[:listing][:title] = listing.title
+
+    data
   end
 
   private
