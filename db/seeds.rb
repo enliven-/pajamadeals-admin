c24b2788299e03bb71fe0f08104797d3d66e5198
Bhushan Lodha <bhushanlodha@gmail.com>
2014-12-09 14:22:20 +0530

    Adding books and listings from spreadsheet

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+def sanitize(e)
+  e.try(:strip).try(:titleize)
+end
+
+# upload books
+
+CSV.foreach("#{Rails.root}/db/book.csv", headers: true) do |row|
+  p sanitize(row[0])
+
+  publication = Publication.find_or_create_by(name: sanitize(row[2]))
+  department = Department.find_or_create_by(name: sanitize(row[6]))
+  semester = Semester.find_or_create_by(name: sanitize(row[8]))
+  subject = Subject.find_or_create_by(name: sanitize(row[9]))
+  university = University.find_or_create_by(name: sanitize(row[10]))
+
+  book = Book.create title: sanitize(row[0]),
+                     authors: sanitize(row[1]),
+                     publication: publication,
+                     book_type: sanitize(row[3]),
+                     mrp: row[5].try(:strip),
+                     department: department,
+                     subject: subject,
+                     university: university
+
+end
+
+CSV.foreach("#{Rails.root}/db/listing.csv", headers: true) do |row|
+  p sanitize(row[0])
+  
+  if row[7].present?
+    college = College.find_or_create_by(name: sanitize(row[8]))
+    user = User.find_or_create_by(mobile: row[7].strip, college: college, name: sanitize(row[6]))
+    if row[1].present? && user.present?
+      listing = Listing.create book_id:  row[1].strip,
+                               user:     user,
+                               edition:  sanitize(row[2]),
+                               quality:  row[3].try(:strip).try(:to_i),
+                               markings: row[4].try(:strip).try(:to_i),
+                               price:    ((row[5].try(:to_i) || 0)*0.55).to_s                     
+     end
+   end
+end
