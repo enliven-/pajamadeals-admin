1b59d6e89768017f1253fac2280be0007f4a0312
Bhushan Lodha <bhushanlodha@gmail.com>
2014-12-12 10:11:03 +0530

    ''

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,6 +41,7 @@ set :bundle_flags, '--system'
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 
 
+
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
