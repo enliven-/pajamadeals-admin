a20adcb44a0db4c9e475f408bf089888068833fb
Bhushan Lodha <bhushanlodha@gmail.com>
2014-12-14 15:58:22 +0530

    Upload images

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,45 +4,55 @@ def sanitize(e)
   e.try(:strip).try(:titleize)
 end
 
-course = Course.create name: 'Bachelor of Engineering', abbr: 'BE'
+# course = Course.create name: 'Bachelor of Engineering', abbr: 'BE'
+#
+# # upload books
+#
+# CSV.foreach("#{Rails.root}/db/book.csv", headers: true) do |row|
+#   p sanitize(row[1])
+#
+#   publication = Publication.find_or_create_by(name: sanitize(row[3]))
+#   department = Department.find_or_create_by(name: sanitize(row[7]))
+#   semester = Semester.find_or_create_by(name: sanitize(row[9]))
+#   subject = Subject.find_or_create_by(name: sanitize(row[10]))
+#   university = University.find_or_create_by(name: sanitize(row[11]))
+#
+#   book = Book.create id: row[0].to_i,
+#                      title: sanitize(row[1]),
+#                      authors: sanitize(row[2]),
+#                      publication: publication,
+#                      book_type: sanitize(row[4]),
+#                      mrp: row[6].try(:strip),
+#                      department: department,
+#                      subject: subject,
+#                      university: university,
+#                      course: Course.first,
+#                      semester: semester
+#
+# end
 
-# upload books
+# images
 
-CSV.foreach("#{Rails.root}/db/book.csv", headers: true) do |row|
-  p sanitize(row[0])
-
-  publication = Publication.find_or_create_by(name: sanitize(row[2]))
-  department = Department.find_or_create_by(name: sanitize(row[6]))
-  semester = Semester.find_or_create_by(name: sanitize(row[8]))
-  subject = Subject.find_or_create_by(name: sanitize(row[9]))
-  university = University.find_or_create_by(name: sanitize(row[10]))
-
-  book = Book.create title: sanitize(row[0]),
-                     authors: sanitize(row[1]),
-                     publication: publication,
-                     book_type: sanitize(row[3]),
-                     mrp: row[5].try(:strip),
-                     department: department,
-                     subject: subject,
-                     university: university,
-                     course: Course.first,
-                     semester: semester
-
+Dir.foreach("#{Rails.root}/db/books1") do |file|
+  next if file == '.' or file == '..'
+  id = file.split('-').first.to_i
+  p "file: #{file}, id: #{id}"
+  book = Book.find id
+  image = Image.create file: File.open(File.join(Rails.root, "db", "books1", file)), book: book
 end
-
-CSV.foreach("#{Rails.root}/db/listing.csv", headers: true) do |row|
-  p sanitize(row[0])
-  
-  if row[7].present?
-    college = College.find_or_create_by(name: sanitize(row[8]), university: University.first)
-    user = User.find_or_create_by(mobile: row[7].strip, college: college, name: sanitize(row[6]))
-    if row[1].present? && user.present?
-      listing = Listing.create book_id:  row[1].strip,
-                               user:     user,
-                               edition:  sanitize(row[2]),
-                               quality:  row[3].try(:strip).try(:to_i),
-                               markings: row[4].try(:strip).try(:to_i),
-                               price:    ((row[5].try(:to_i) || 0)*0.55).round.to_s                     
-     end
-   end
-end
+#
+# CSV.foreach("#{Rails.root}/db/listing.csv", headers: true) do |row|
+#   p sanitize(row[1])
+#
+#   if row[9].present? && row[2].present?
+#     college = College.find_or_create_by(name: sanitize(row[10]), university: University.first)
+#     user = User.find_or_create_by(mobile: row[9].strip, college: college, name: sanitize(row[8]))
+#     listing = Listing.create id: row[0].strip.to_i,
+#                              book_id:  row[2].strip,
+#                              user:     user,
+#                              edition:  sanitize(row[3]),
+#                              quality:  row[4].try(:strip).try(:to_i),
+#                              markings: row[5].try(:strip).try(:to_i),
+#                              price:    ((row[7].try(:to_i) || 0)*0.50).round.to_s
+#    end
+# end
